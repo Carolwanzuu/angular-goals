a92e8c3449ba3880d3b64370a27781dcf9f1e7bb
Carolwanzuu <carolkyalo01@gmail.com>
2021-05-13 07:05:09 +0300

    created an angular app

---
+++ src/app/goal/goal.component.ts
@@ -0,0 +1,71 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Goal } from '../goal';
+import { GoalService } from '../goal-service/goal.service';
+import { AlertService } from '../alert-service/alert.service';
+import { Quote } from '../quote-class/quote';
+import { QuoteRequestService } from '../quote-http/quote-request.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-goal',
+  templateUrl: './goal.component.html',
+  styleUrls: ['./goal.component.css']
+})
+export class GoalComponent implements OnInit {
+
+  
+  goals: Goal[];
+  alertService: AlertService;
+  quote: Quote;
+
+  goToUrl(id) {
+    this.router.navigate(['/goals', id])
+  }
+
+  addNewGoal(goal) {
+    let goalLength = this.goals.length;
+    goal.id = goalLength + 1;
+    goal.completeDate = new Date(goal.completeDate)
+    this.goals.push(goal)
+  }
+  //   toggleDetails(index){
+  //     this.goals[index].showDescription = !this.goals[index].showDescription;
+
+  //   }completeGoal(isComplete, index){
+  //     if (isComplete){
+  //       this.goals.splice(index, 1);
+  //     }
+  //   }
+  deleteGoal(index) {
+
+    let toDelete = confirm(`Are you sure you want to delete ${this.goals[index].name}?`)
+
+    if (toDelete) {
+      this.goals.splice(index, 1)
+      this.alertService.alertMe("The goal has been deleted")
+
+    }
+  }
+
+constructor(goalService: GoalService, alertService: AlertService, private quoteService: QuoteRequestService, private router: Router){
+  this.goals = goalService.getGoals()
+  this.alertService = alertService;
+}
+ngOnInit(): void {
+  this.quoteService.quoteRequest()
+     this.quote = this.quoteService.quote
+  //  interface ApiResponse{
+  //    author:string;
+  //    quote:string;
+  //  }
+  //  this.http.get<ApiResponse>("http://quotes.stormconsultancy.co.uk/random.json").subscribe(data=>{
+  //    this.quote = new Quote(data.author, data.quote)
+  //  }, err=>{
+  //   this.quote = new Quote("Winston Churchill","Never never give up!")
+  //   console.log("An error occurred")
+
+
+}
+}
+
